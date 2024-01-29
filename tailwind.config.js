fa1a6a984acefa37a7caedcbf37340e3d594e5fa
OliverkNilsson <oliverknilsson@gmail.com>
2024-01-29 13:48:18 +0100

    Whole project commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./singleproduct.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'green': '#1B6713',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    },
+  },
+  plugins: [],
+}
+
